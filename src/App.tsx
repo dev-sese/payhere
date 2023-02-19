1005b4595f6637a87dd570aeb5aa16debd98297e
dev-sese <dev.shhong@gmail.com>
2023-02-20 02:49:39 +0900

    검색 페이지 분리 및 localStorage에 샘플 저장

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { getRepositoriesWithSearch } from "services/Search.service";
+import SearchPage from "pages/SearchPage";
 import "./App.css";
 
 function App() {
-  const [searchInput, setSearchInput] = useState("");
-
-  const [searchParam, setSearchParam] = useState("#");
-
-  const [apiresponse, setApiresponse] = useState<any>([]);
-
-  const searchParamChangeHandler = () => {
-    setSearchParam(searchInput);
-  };
-
-  useEffect(() => {
-    getRepositoriesWithSearch(searchParam)
-      .then((response) => {
-        setApiresponse(response);
-      })
-      .catch((error) => {
-        throw error;
-      });
-  }, [searchParam]);
-
   return (
     <div className="App">
       <h1>페이히어 프론트엔드 엔지니어 과제</h1>
-      <div>
-        <input
-          type={"text"}
-          value={searchInput}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            setSearchInput(e.target.value);
-          }}
-        />
-        <button onClick={searchParamChangeHandler}>검색</button>
-        <div>우히히</div>
-        <div>
-          {searchInput} {searchParam}
-        </div>
-        <div>
-          {apiresponse.length !== 0 &&
-            //
-            apiresponse.map((data: any) => {
-              return <div key={data.id}>{data["full_name"]}</div>;
-            })}
-        </div>
-      </div>
+      <SearchPage />
     </div>
   );
 }
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,1 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getRepositoriesWithSearch } from "services/Search.service";
+
+const SearchPage: React.FC = () => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const [searchParam, setSearchParam] = useState("#");
+
+  const [apiresponse, setApiresponse] = useState<any>([]);
+
+  const searchParamChangeHandler = () => {
+    setSearchParam(searchInput);
+  };
+
+  useEffect(() => {
+    getRepositoriesWithSearch(searchParam)
+      .then((response) => {
+        setApiresponse(response);
+        response?.slice(0, 4).map((data) => {
+          window.localStorage.setItem(data.id + "", JSON.stringify(data));
+        });
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }, [searchParam]);
+
+  return (
+    <div>
+      <input
+        type={"text"}
+        value={searchInput}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setSearchInput(e.target.value);
+        }}
+      />
+      <button onClick={searchParamChangeHandler}>검색</button>
+      <div>우히히</div>
+      <div>
+        {searchInput} {searchParam}
+      </div>
+      <div>
+        {apiresponse.length !== 0 &&
+          //
+          apiresponse.map((data: any) => {
+            return <div key={data.id}>{data["full_name"]}</div>;
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default SearchPage;
