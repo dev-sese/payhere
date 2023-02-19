de3ea9f899ed0a5066fbf7304f88ae84cb66c9dc
dev-sese <dev.shhong@gmail.com>
2023-02-19 23:48:51 +0900

    octokit 라이브러리 추가 및 예시 API 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import "src/App.css";
+import { useEffect } from "react";
+import { getRepositoriesWithSearch } from "./services/Search.service";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    let response = getRepositoriesWithSearch();
+
+    console.log(response);
+  }, []);
+
   return <div className="App">test page</div>;
 }
 
--- src/services/Search.service.ts
+++ src/services/Search.service.ts
@@ -1,1 +1,11 @@
-import React from "react";
+import { Octokit } from "octokit";
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_ACCESS_TOKEN,
+});
+
+export const getRepositoriesWithSearch = async () => {
+  let options: any = { q: "test" };
+  let response = await octokit.request("GET /search/repositories", options);
+  return response;
+};
