4ca7c505432fc202b087946bfc9fd53ecd5429fe
dev-sese <dev.shhong@gmail.com>
2023-02-20 02:43:35 +0900

    repository 검색 API 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,54 @@
-import { useEffect } from "react";
-import { getRepositoriesWithSearch } from "./services/Search.service";
+import React, { useEffect, useState } from "react";
+import { getRepositoriesWithSearch } from "services/Search.service";
 import "./App.css";
 
 function App() {
+  const [searchInput, setSearchInput] = useState("");
+
+  const [searchParam, setSearchParam] = useState("#");
+
+  const [apiresponse, setApiresponse] = useState<any>([]);
+
+  const searchParamChangeHandler = () => {
+    setSearchParam(searchInput);
+  };
+
   useEffect(() => {
-    let response = getRepositoriesWithSearch();
+    getRepositoriesWithSearch(searchParam)
+      .then((response) => {
+        setApiresponse(response);
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }, [searchParam]);
 
-    console.log(response);
-  }, []);
-
-  return <div className="App">test page</div>;
+  return (
+    <div className="App">
+      <h1>페이히어 프론트엔드 엔지니어 과제</h1>
+      <div>
+        <input
+          type={"text"}
+          value={searchInput}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setSearchInput(e.target.value);
+          }}
+        />
+        <button onClick={searchParamChangeHandler}>검색</button>
+        <div>우히히</div>
+        <div>
+          {searchInput} {searchParam}
+        </div>
+        <div>
+          {apiresponse.length !== 0 &&
+            //
+            apiresponse.map((data: any) => {
+              return <div key={data.id}>{data["full_name"]}</div>;
+            })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/services/Search.service.ts
+++ src/services/Search.service.ts
@@ -4,8 +4,14 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_ACCESS_TOKEN,
 });
 
-export const getRepositoriesWithSearch = async () => {
-  let options: any = { q: "test" };
-  let response = await octokit.request("GET /search/repositories", options);
-  return response;
+export const getRepositoriesWithSearch = async (searchParam: string) => {
+  try {
+    let options: any = {
+      q: `${searchParam === "" ? "#" : searchParam}+in:name`,
+    };
+    let response = await octokit.request("GET /search/repositories", options);
+    return response.data.items;
+  } catch (error) {
+    alert(error);
+  }
 };
