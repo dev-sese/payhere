3c8b83cab0b7390d17eb15359707a823f92c1517
dev-sese <dev.shhong@gmail.com>
2023-02-20 21:44:11 +0900

    repository 검색 결과 분리 및 개수제한 알림 추가

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,1 +1,69 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
+import { Checkbox } from "primereact/checkbox";
+import { Toast } from "primereact/toast";
+
+interface Category {
+  id: number;
+  name: string;
+  key: string;
+}
+
+const SearchResult = ({ repoList }: any) => {
+  const toast = useRef<Toast>(null);
+  const [selectedCategories, setSelectedCategories] = useState<Category[]>([]);
+
+  const onCategoryChange = (e: any) => {
+    let _selectedCategories = [...selectedCategories];
+
+    if (e.checked) {
+      _selectedCategories.push(e.value);
+    } else {
+      _selectedCategories = _selectedCategories.filter(
+        (category) => category.id !== e.value.id
+      );
+    }
+
+    if (_selectedCategories.length < 5) {
+      setSelectedCategories(_selectedCategories);
+    } else {
+      toast.current?.show({
+        severity: "warn",
+        summary: "등록제한",
+        detail: "repository 등록은 4개를 넘길 수 없어요",
+        life: 3000,
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (selectedCategories.length !== 0) {
+      localStorage.clear();
+      localStorage.setItem("checkedRepo", JSON.stringify(selectedCategories));
+    }
+  }, [selectedCategories]);
+
+  return (
+    <div>
+      <Toast ref={toast} />
+      {repoList.length !== 0 &&
+        repoList.map((data: any) => {
+          return (
+            <div key={data.id} className="flex align-items-center">
+              <Checkbox
+                inputId={data.id}
+                name="category"
+                value={data}
+                onChange={onCategoryChange}
+                checked={selectedCategories.some((item) => item.id === data.id)}
+              />
+              <label htmlFor={data.id} className="ml-2">
+                {data["full_name"]}
+              </label>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+export default SearchResult;
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,64 +1,6 @@
+import SearchResult from "components/SearchResult";
 import React, { useEffect, useState } from "react";
 import { getRepositoriesWithSearch } from "services/Search.service";
-import { Checkbox } from "primereact/checkbox";
-import { Toast } from "primereact/toast";
-
-interface Category {
-  id: number;
-  name: string;
-  key: string;
-}
-
-const Items = ({ repoList }: any) => {
-  const [selectedCategories, setSelectedCategories] = useState<Category[]>([]);
-
-  const onCategoryChange = (e: any) => {
-    let _selectedCategories = [...selectedCategories];
-
-    if (e.checked) {
-      _selectedCategories.push(e.value);
-    } else {
-      _selectedCategories = _selectedCategories.filter(
-        (category) => category.id !== e.value.id
-      );
-    }
-
-    if (_selectedCategories.length < 5) {
-      setSelectedCategories(_selectedCategories);
-    } else {
-      console.log("4개 넘어갔어요!!");
-    }
-  };
-
-  useEffect(() => {
-    if (selectedCategories.length !== 0) {
-      localStorage.clear();
-      localStorage.setItem("checkedRepo", JSON.stringify(selectedCategories));
-    }
-  }, [selectedCategories]);
-
-  return (
-    <div>
-      {repoList.length !== 0 &&
-        repoList.map((data: any) => {
-          return (
-            <div key={data.id} className="flex align-items-center">
-              <Checkbox
-                inputId={data.id}
-                name="category"
-                value={data}
-                onChange={onCategoryChange}
-                checked={selectedCategories.some((item) => item.id === data.id)}
-              />
-              <label htmlFor={data.id} className="ml-2">
-                {data["full_name"]}
-              </label>
-            </div>
-          );
-        })}
-    </div>
-  );
-};
 
 const SearchPage: React.FC = () => {
   const [searchInput, setSearchInput] = useState("");
@@ -100,7 +42,7 @@ const SearchPage: React.FC = () => {
       <div>
         {apiresponse.length !== 0 && (
           //
-          <Items repoList={apiresponse} />
+          <SearchResult repoList={apiresponse} />
         )}
       </div>
     </div>
